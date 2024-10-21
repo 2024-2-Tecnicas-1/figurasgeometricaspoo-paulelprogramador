5b8efc5ccb9d794e5599c3b9bd86c912f35c8ecd
paulelprogramador <paulsantiago114@gmail.com>
2024-10-20 23:50:10 -0500

    Update Circulo.java

--- src/main/java/com/mycompany/figurasgeometricas/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -1,1 +1,34 @@
-crear clase circulo
+package com.mycompany.figurasgeometricas;
+
+
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    // Complejidad: O(1)
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    // Complejidad: O(1)
+    @Override
+    public double obtenerArea() {
+        return 3.1415926 * radio * radio;
+    }
+
+    // Complejidad: O(1)
+    @Override
+    public double obtenerPerimetro() {
+        return 2 * Math.PI* radio;
+    }
+
+    // Complejidad: O(1)
+    public double getRadio() {
+        return radio;
+    }
+
+    // Complejidad: O(1)
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
