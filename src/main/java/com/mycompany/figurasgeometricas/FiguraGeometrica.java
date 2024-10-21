ef3275d1138c2d99e8d75590acd8d240b51fc041
paulelprogramador <paulsantiago114@gmail.com>
2024-10-20 23:49:46 -0500

    Update FiguraGeometrica.java

--- src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
@@ -1,1 +1,38 @@
-a
+
+package com.mycompany.figurasgeometricas;
+
+
+public class FiguraGeometrica {
+
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    // Complejidad: O(1)    public double obtenerArea() {
+        return 0;
+    }
+
+    // Complejidad: O(1)
+    public double obtenerPerimetro() {
+        return 0;
+    }
+
+    // Complejidad: O(1)
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Complejidad: O(1)
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Complejidad: O(1)
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
