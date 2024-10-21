28e12ba1559783bc72ebe62be08843e8be29c312
paulelprogramador <paulsantiago114@gmail.com>
2024-10-20 23:51:01 -0500

    Update Triangulo.java

--- src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/Triangulo.java
@@ -1,1 +1,34 @@
-a
+package com.mycompany.figurasgeometricas;
+
+public class Triangulo extends FiguraGeometrica {
+
+    private double lado;
+
+    
+    public Triangulo(String nombre, String color, double lado) {
+        super(nombre, color);
+        this.lado = lado;
+    }
+
+    // Complejidad: O(1)
+    @Override
+    public double obtenerArea() {
+        return 0.433 * lado * lado;
+    }
+
+    // Complejidad: O(1)
+    @Override
+    public double obtenerPerimetro() {
+        return 3 * lado;
+    }
+
+    // Complejidad: O(1)
+    public double getLado() {
+        return lado;
+    }
+
+    // Complejidad: O(1)
+    public void setLado(double lado) {
+        this.lado = lado;
+     }
+}
